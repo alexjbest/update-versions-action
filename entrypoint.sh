d43e4b09d51c15aefb0b81699db73c63bb02fb6a
Rob Lewis <rob.y.lewis@gmail.com>
2020-08-02 14:12:58 +0200

    shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -l
+
 branch_name="lean-$(grep -oP 'lean_version = \"leanprover-community\/lean\:\K[^\=]+(?=\")' leanpkg.toml)"
 echo "Updating mathlib branch $branch_name to match master"
 git push "$1" HEAD:refs/heads/$branch_name
